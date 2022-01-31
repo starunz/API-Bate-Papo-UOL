74549426f038df07bbc3f87248c80f1a506edbd1
starunz <busouza01@gmail.com>
2022-01-31 11:16:26 -0500

    post participants

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
+import joi from "joi";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,6 +14,65 @@ app.use(cors());
 
 app.listen(5000, () => {
     console.log('rodando')
+});
+
+const nameSchema = joi.object({
+    name: joi.string().case('lower').required(),
+});
+
+app.post('/participants', async (req, res) => {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+  
+    const validation = nameSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+      const errors = validation.error.details.map(detail => detail.message);
+      res.status(422).send(errors);
+      return;
+    }
+
+    try {
+        await mongoClient.connect();
+
+        const participants = await mongoClient
+        .db(process.env.MONGO_NAME)
+        .collection('participants')
+        .find({})
+        .toArray();
+    
+        const existingUser = participants.find(user => user.name.toLowerCase() === req.body.name.toLowerCase());
+    
+        if (existingUser) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await mongoClient
+        .db(process.env.MONGO_NAME)
+        .collection('participants')
+        .insertOne({
+            name: req.body.name,
+            lastStatus: Date.now(),
+        });
+
+        await mongoClient
+        .db(process.env.MONGO_NAME)
+        .collection('messages')
+        .insertOne({
+        from: req.body.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`,
+        });
+
+        res.sendStatus(201);
+
+        mongoClient.close();
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error);
+    }
 });
 
 app.get('/participants', async (req, res) => {
